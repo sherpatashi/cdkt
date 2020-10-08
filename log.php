4d905570ba7060a6309c039cbc11009de02f9ba4
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-08 15:30:35 -0400

    test

---
+++ log.php
@@ -0,0 +1,32 @@
+<?php
+<?php
+/* Displays all error messages */
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Error</title>
+  <?php include 'css/css.html'; ?>
+</head>
+<body>
+<div class="form">
+    <h1>Error</h1>
+    <p>
+    <?php
+    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ):
+        echo $_SESSION['message'];
+        $date = date_create();
+        file_put_contents('/home/tashid/git/log/', "[".date_format($date, 'm-d-Y H:i:s')."] ".$_SESSION['message'].PHP_EOL, FILE_APPEND);
+    else:
+        header( "location: index.php" );
+    endif;
+    ?>
+    </p>
+    <a href="index.php"><button class="button button-block"/>Home</button></a>
+</div>
+</body>
+</html>
+
+
+?>
