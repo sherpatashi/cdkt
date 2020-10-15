f633a3388ace5d236c3ea4ec2d1406fcad4d9bf8
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-15 14:49:59 -0400

    update

--- log.php
+++ log.php
@@ -1,32 +1,27 @@
+#!/usr/bin/php
 <?php
-<?php
-/* Displays all error messages */
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Error</title>
-  <?php include 'css/css.html'; ?>
-</head>
-<body>
-<div class="form">
-    <h1>Error</h1>
-    <p>
-    <?php
-    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ):
-        echo $_SESSION['message'];
-        $date = date_create();
-        file_put_contents('/home/tashid/git/log/', "[".date_format($date, 'm-d-Y H:i:s')."] ".$_SESSION['message'].PHP_EOL, FILE_APPEND);
-    else:
-        header( "location: index.php" );
-    endif;
-    ?>
-    </p>
-    <a href="index.php"><button class="button button-block"/>Home</button></a>
-</div>
-</body>
-</html>
 
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+	
 
+function logErrors( $error_number, $error_string, $filename, $line_number )
+{
+        echo $error_number . "<br>";
+        echo $error_string . "<br>";
+        echo $filename . "<br>";
+	echo $line_number . "<br>";
+	echo $today . "<br>";
+	echo $time . "<br>";
+$today = date("m-Y-d");
+$time =  date("h:i:sa");
+$myfile = "logs";
+$file = " [" . $today . " | " . $time . "]  " . $error_number ." :" . $error_string . " in " . $filename . " at line " . $line_number . "\n";
+
+file_put_contents("log/" . $myfile . ".txt", $file, FILE_APPEND);
+
+}
+
+set_error_handler("logErrors");
 ?>
