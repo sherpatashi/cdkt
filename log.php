993614a352d40af82935cadaa5ff4521924b2d9a
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-19 17:52:41 -0400

    updated

--- log.php
+++ log.php
@@ -8,13 +8,6 @@
 //this function log errrors.
 function logErrors( $error_number, $error_string, $filename, $line_number )
 {
-	echo $error_number . "<br>";
-	echo $error_string . "<br>"; 
-	echo $filename . "<br>";
-	echo $line_number . "<br>";
-	echo $today . "<br>";
-	echo $time . "<br>";
-
 	
 $today = date("m-Y-d");
 $time =  date("h:i:sa");
@@ -23,27 +16,6 @@ $file = " [" . $today . " | " . $time . "]  " . $error_number ." :" . $error_str
 
 file_put_contents("log/" . $myfile . ".txt", $file, FILE_APPEND);
 
-}
-set_error_handler("logErrors");
 
-
-//this function check the responds.
-function if_responds($message)
-{
-$message = strtolower($message);
-  if (preg_match('/successfully/',$message) | preg_match('/failed/',$message))
-  {
-   	 return True;
-  }
-  	return false;
-}
-// this line send log to server
-if (if_responds($message)
-{
-	$client = new rabbitMQClient("errorListener.ini","testServer");
-    	$client->publish($message);	
-}
-
-error_log($message.PHP_EOL,3,"home/tashid/changeRabbit/log/logs.txt");
 ?>
 
