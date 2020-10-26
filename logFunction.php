deb9c35b864cee62c37c269353c2fa38ec3a09be
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-26 18:13:22 -0400

    update logging system

--- logFunction.php
+++ logFunction.php
@@ -1,101 +1,66 @@
 #!/usr/bin/php
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-//error detection
 function logErrors($error_level, $error_message, $filename, $line_number){
-	$today = date("M/d/Y");
-	$time = date("h:i:sa");
-	//$myfile = "logs";
+  	date_default_timezone_set('America/New_York');
+	$time = date("M/d/Y | h:i:sa");
+	$file  = "[$time]  $error_level in $filename at line $line_number. \n";
 
-	$file  = "[$today | $time]  $error_level in $filename at line $line_number. \n";
-	publishToLog($file);
-	//file_put_contents("log/$myfile.log", $file, FILE_APPEND);
-
+  $client = new rabbitMQClient("logRMQ.ini","logServer");
+	$request = array();
+	$request['type'] = "log-error";
+  	$request['message'] = $file;
+  
+	$response = $client->send_request($request);
+	#$response = $client->publish($request);
+ 	return $reponse;
 }
 set_error_handler("logErrors");
-
+	
 // Login logging system.
+function LogLogin($log){
+  	date_default_timezone_set('America/New_York');
+	$time = date("M/d/Y | h:i:sa");
+  	$file  = "[$time]  $log \n";
 
-//function ifNull($username,$password){
-
-$username = strtolower($_GET['username']);
-$password = strtolower($_GET['password']);
-
-if($username == '' || $password == '' || $username == null || $password == null){
-	/*
-	$logFile = fopen("logs/login.log", "a");
-	$logText = "Login attempt failed due to null entry.\n";
-	fwrite($logFile, $logText);
-  	fclose($logFile);
-	*/
-	$logText = "Login attempt failed due to null entry.\n";
-	publishToLog($$logText);
-}
-$response = login($username, $password);
-if($response == true){
-	$_SESSION['username'] = $username;
-	/*
-	$logFile = fopen("logs/login.log", "a");
-	$logText = "[$today | $time] Login attempt from User: $username | Login Successful.\n";
-
-	fwrite($logFile, $logText);
-	fclose($logFile);
- 	*/
-	$logText = "[$today | $time] Login attempt from User: $username | Login Successful.\n";
-	publishToLog($logText);
+  	$client = new rabbitMQClient("logRMQ.ini","logServer");
+	$request = array();
+	$request['type'] = "log-login";
+  	$request['message'] = $file;
+  
+	$response = $client->send_request($request);
+	#$response = $client->publish($request);
+  	return $reponse;
 }
 
-// Registration logging system.
+//registration loggin system.
+function LogRegister($log){
+  	date_default_timezone_set('America/New_York');
+	$time = date("M/d/Y | h:i:sa");
+  	$file  = "[$time]  $log \n";
+  
+  	$client = new rabbitMQClient("logRMQ.ini","logServer");
+	$request = array();
+	$request['type'] = "log-register";
+  	$request['message'] = $file;
+  
+	$response = $client->send_request($request);
+	#$response = $client->publish($request);
+  	return $reponse; 
+}
+  
+  //SQL logging system.
+function logSQL($log){
+  	date_default_timezone_set('America/New_York');
+	$time = date("M/d/Y | h:i:sa");
+ 	 $file  = "[$time]  $log \n";
+  
+  	$client = new rabbitMQClient("logRMQ.ini","logServer");
+	$request = array();
+	$request['type'] = "log-SQL";
+  	$request['message'] = $file;
 
-//$regUsername = strtolower($_GET['registerUsername']);
-//$regPassword = strtolower($_GET['registerPassword']);
-
-if($regUsername == '' || $regPassword == '' || $regUsername == null || $regPassword == null){
-        echo "Username or Password field left empty, try again.";
-        header("location:../index.html?registrationEmpty=blank");
-	/*
-	$logFile = fopen("logs/accountRegistration.log", "a");
-        $logText = "[$today | $time] Account registration attempt failed due to null entry.\n";
-      	fwrite($logFile, $logText);
-	*/
-	$logText = "[$today | $time] Account registration attempt failed due to null entry.\n";fclose($logFile);
-      	publishToLog($logText);
+	$response = $client->send_request($request);
+	#$response = $client->publish($request);
+  	return $reponse; 
 }
 
-$response = registration($regUsername, $regPassword);
-
-if($response == true){
-	echo "\nAccount has been created successfully.";
-	#  header REFRESH THE LOGIN WITH SUCCESSFUL ACCOUNT CREATION NOTIFICATION.
-	/*
-	$logFile = fopen("logs/accountRegistration.log", "a");
-        $logText = "[$today | $time] Account registration attempt for User: $regUsername & Password: $regPassword | Registration successful.\n";
-	fwrite($logFile, $logText);
-        fclose($logFile);
-	*/
-        $logText = "[$today | $time] Account registration attempt for User: $regUsername & Password: $regPassword | Registration successful.\n";
-	publishToLog($logText);
-}
-elseif($response == false){
-	echo "\nAccount already exists.";
-        #  header REFRESH THE LOGIN WITH SUCCESSFUL ACCOUNT CREATION NOTIFICATION.
-	/*
-        $logFile = fopen("logs/accountRegistration.log", "a");
-        $logText = "[$today | $time] Account registration attempt for User: $regUsername & Password: $regPassword | Failed because username already exists.\n";
-        fwrite($logFile, $logText);
-        fclose($logFile);
-	*/
-	$logText = "[$today | $time] Account registration attempt for User: $regUsername & Password: $regPassword | Failed because username already exists.\n";
-      	publishToLog($logText);
-}
-
-//send log message to the server.
-function publishToLog($message){
-        $client = new rabbitMQClient('logRMQ.ini','logServer');
-        $client->publish($message);
-        
-}
 ?>
