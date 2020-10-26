f3477c65e79a5167485f7cad71fdba773cb991e8
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-26 18:23:48 -0400

    update logging  system.

--- logListener.php
+++ logListener.php
@@ -1,19 +1,44 @@
 #!/usr/bin/php
 <?php
-  require_once('path.inc');
-  require_once('get_host_info.inc');
-  require_once('rabbitMQLib.inc');
-	
-  $server = new rabbitMQServer('logRMQ.ini', 'logServer');
-  $server->process_message('messageProcessor');
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-	function messageProcessor($message){
-        echo "received message".PHP_EOL;
-        echo $message; 
-  }
-  //file_put_contents('frontEnd.log', $message);
+     // creates file and store logs.
+function storeLogs($message, $fileName){
+	$fp = fopen( $fileName . '.log', "a" );
+        fwrite( $fp, $message);
+        fclose($fp);
+       	return true;
+}
+//route the request from client.
+function requestProcessor($request){
+  	echo "Received Request:\n\n";
+	var_dump($request);
 
-$logFile = fopen("logs/LogSystem.log", "a");
-	fwrite($logFile, $message);
-  fclose($logFile);
+	if(!isset($request['type'])){
+    		return "ERROR: unsupported message type";
+	}
+
+switch ($request['type']){
+	case "log-error":
+      		$msg =  logErrors($request['message'], "logError");
+		
+    	case "log-login":
+		$msg = storeLogs($request['message'], "logLogin");
+		
+	case "log-register":
+		$msg =  logRegister($request['message'], "logRegister");
+    	case "log-SQL":
+      		$msg =  logSQL($request['message'], "logSQL");
+	}
+	return $msg;
+}
+//creating a new server
+$server = new rabbitMQServer('RMQ.ini', 'logServer');
+//processes the request sent by client
+$server->process_requests('requestProcessor');
+   
+
+
 ?>
