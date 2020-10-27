fa73d87c6d0589540d443a9e6b29220224f47cc7
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-26 21:12:25 -0400

    update logging system

--- logFunction.php
+++ logFunction.php
@@ -1,5 +1,11 @@
 #!/usr/bin/php
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors,' 1)
+
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -53,20 +59,4 @@ function LogRegister($log){
   	return $reponse; 
 }
   
- 	//SQL logging system.
-function logSQL($log){
-  	date_default_timezone_set('America/New_York');
-	$time = date("M/d/Y | h:i:sa");
- 	 $file  = "[$time]  $log \n";
-  
-  	$client = new rabbitMQClient("logRMQ.ini","logServer");
-	$request = array();
-	$request['type'] = "log-SQL";
-  	$request['message'] = $file;
-
-	$response = $client->send_request($request);
-	#$response = $client->publish($request);
-  	return $reponse; 
-}
-
 ?>
