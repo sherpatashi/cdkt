5daeb476d89383f830b8377f46f8c3f08ffb097b
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-10-30 16:45:09 -0400

    update logging system

--- logClient.php
+++ logClient.php
@@ -1,13 +1,13 @@
 #!/usr/bin/php
 <?php
 
-equire_once('path.inc');
+require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 //Error logging system.
 function logErrors($error_level, $error_string, $filename, $line_number){
-
+	
 	date_default_timezone_set('America/New_York');
 	$time = date("M/d/Y | h:i:sa");
 	$file  = "[$time] Error: $error_level, $error_string in $filename at line $line_number. \n";
@@ -16,28 +16,45 @@ function logErrors($error_level, $error_string, $filename, $line_number){
 	$request = array();
 	$request['type'] = "log-error";
   	$request['message'] = $file;
-
+  
 	$response = $client->send_request($request);
 	#$response = $client->publish($request);
  	return $response;
 }
 
 
-//SQL logging system.
-function logSQL($log){
+// Login logging system.
+function LogLogin($log){
 
   	date_default_timezone_set('America/New_York');
 	$time = date("M/d/Y | h:i:sa");
- 	$file  = "[ $time ]  $log ";
+  	$file  = "[ $time ]  $log ";
+
+  	$client = new rabbitMQClient("testRabbitMQ.ini","logServer");
+	$request = array();
+	$request['type'] = "log-login";
+  	$request['message'] = $file;
+  
+	$response = $client->send_request($request);
+	#$response = $client->publish($request);
+  	return $response;
+}
+
+//registration loggin system.
+function LogRegister($log){
+
+  	date_default_timezone_set('America/New_York');
+	$time = date("M/d/Y | h:i:sa");
+  	$file  = "[ $time ]  $log ";
   
   	$client = new rabbitMQClient("testRabbitMQ.ini","logServer");
 	$request = array();
-	$request['type'] = "log-SQL";
+	$request['type'] = "log-register";
   	$request['message'] = $file;
-
+  
 	$response = $client->send_request($request);
 	#$response = $client->publish($request);
   	return $response; 
 }
- 
+  
 ?>
