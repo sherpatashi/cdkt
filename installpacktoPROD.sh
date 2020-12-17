6adb2d7801ddbdc8e1424ec832864317968fe57d
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-12-17 01:30:42 -0500

    update deployment script

--- installpacktoPROD.sh
+++ installpacktoPROD.sh
@@ -2,9 +2,8 @@
 sleep 1
 echo "************Installing Package.v2 in the Production Environment***********************"
 echo " "
-#sshpass -p 'Greenapples9---' scp -r /home/deployment/Package.v2/  @25.104.222.167:/var/www
 sudo sshpass -p 'Orangerice' rsync -rz /home/deployment/Package.v2/ tashidsherpa@25.68.236.97:/var/www
 sleep 3
 echo "****************Install Package.v2 Completed. Now..Production is Rebooting *******************"
 echo " "
-sleep 4
+sleep 3
