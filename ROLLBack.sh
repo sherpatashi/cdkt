5472abe4c911c69a5c7fa7a90546adc21b604899
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-12-17 01:46:28 -0500

    update deployment system

--- ROLLBack.sh
+++ ROLLBack.sh
@@ -11,15 +11,22 @@ if [ "$answer" = "y" ]; then
 	echo " "
 	
 	
-
-	#sshpass -p 'Greenapples9---' carloscaldero@25.104.222.167; sudo rm -rf /var/www/html
-	#sshpass -p 'Greenapples9---' scp -r /home/delpoyment/package.v1/ @25.104.222.167:/var/www/html
-	sleep 4
-
+	sudo sshpass -p 'Orangerice' ssh tashidsherpa@25.68.236.97 "sh -c 'rm -rf /var/www/*'"
+	sleep 2
+	sudo sshpass -p 'Orangerice' rsync -rz /home/deployment/Package.v1/Pv1.tar.gz tashidsherpa@25.68.236.97:/var/www
+	sudo sshpass -p 'Orangerice' ssh -t tashidsherpa@25.68.236.97 '. ~/.barshrc; cd /var/www; tar -xvzf Pv2.tar.gz'
+	sleep 2
+	clear
+	echo " "
+	echo "************Rolling Back to the Package.v1.************"
+	echo " "
+	echo " "
+	sleep 2.6
 	echo "************Roll Back Completed. Now.. Production is Rebooting..***********"
 	echo " "
 	sleep 3
 elif [ "$answer" = "n" ]; then 
+	
+	sleep 1.5
 	exit 
-
 fi
