334ab7dcd52e498ee0215e88680787e1bb210087
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-12-17 01:41:53 -0500

    update deployment system

--- installpacktoPROD.sh
+++ installpacktoPROD.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 sleep 1
+echo " "
 echo "************Installing Package.v2 in the Production Environment***********************"
 echo " "
-sudo sshpass -p 'Orangerice' rsync -rz /home/deployment/Package.v2/ tashidsherpa@25.68.236.97:/var/www
-sleep 3
+sudo sshpass -p 'Orangerice' rsync -rz /home/deployment/Package.v2/Pv2.tar.gz tashidsherpa@25.68.236.97:/var/www
+sudo sshpass -p 'Orangerice' ssh -t tashidsherpa@25.68.236.97 '. ~/.barshrc; cd /var/www; tar -xvzf Pv2.tar.gz'
+sleep 2
+clear 
+echo " "
+echo "************Installing Package.v2 in the Production Environment***********************"
+echo " "
+sleep 2.5
+echo " "
 echo "****************Install Package.v2 Completed. Now..Production is Rebooting *******************"
 echo " "
-sleep 3
+sleep 2.5
