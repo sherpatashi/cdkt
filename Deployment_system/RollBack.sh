25b11a08f08d5b161c1a5404c32b7cc992e7b265
sherpatashi <70527736+sherpatashi@users.noreply.github.com>
2020-12-17 21:43:58 -0500

    updated deployment script

--- Deployment_system/RollBack.sh
+++ Deployment_system/RollBack.sh
@@ -9,12 +9,12 @@ if [ "$answer" = "y" ]; then
 	echo "************Rolling Back to the Package.v1.************"
 	echo " "
 	echo " "
-	
-	
-	sudo sshpass -p 'Orangerice' ssh tashidsherpa@25.68.236.97 "sh -c 'rm -rf /var/www/*'"
+
+
+	sudo sshpass -p '8Saibaba' ssh divyap@25.96.32.28 "sh -c 'rm -rf /var/www/*'"
 	sleep 2
-	sudo sshpass -p 'Orangerice' rsync -rz /home/deployment/Package.v1/Pv1.tar.gz tashidsherpa@25.68.236.97:/var/www
-	sudo sshpass -p 'Orangerice' ssh -t tashidsherpa@25.68.236.97 '. ~/.barshrc; cd /var/www; tar -xvzf Pv2.tar.gz'
+	sudo sshpass -p '8Saibaba' rsync -rz /home/deployment/Package.v1/Pv1.tar.gz divyap@25.96.32.28:/var/www
+	sudo sshpass -p '8Saibaba' ssh -t divyap@25.96.32.28'. ~/.barshrc; cd /var/www; tar -xvzf Pv1.tar.gz'
 	sleep 2
 	clear
 	echo " "
@@ -25,8 +25,8 @@ if [ "$answer" = "y" ]; then
 	echo "************Roll Back Completed. Now.. Production is Rebooting..***********"
 	echo " "
 	sleep 3
-elif [ "$answer" = "n" ]; then 
-	
+elif [ "$answer" = "n" ]; then
+
 	sleep 1.5
-	exit 
-fi
+	exit
+fil
